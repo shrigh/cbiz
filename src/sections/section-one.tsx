21a0bfa2fdaf2f4ede7e549b027ecb188aea9983
Aquib349 <aquib_m@optimusbt.net>
2024-11-26 17:29:25 +0530

    new code

--- src/sections/section-one.tsx
+++ src/sections/section-one.tsx
@@ -12,7 +12,7 @@ const SectionOne: React.FC = () => {
     // Simulate loader progress
     const progressInterval = setInterval(() => {
       setProgress((prev) => (prev < 100 ? prev + 1 : 0));
-    }, 50);
+    }, 40);
 
     if (progress === 100) {
       setProgress(0);
